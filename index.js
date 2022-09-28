ac0e8a89f13defec9ff14ad3fd7dba2034a17e2f
DDaniel-Dev <ddaniel.atx@gmail.com>
2022-09-28 14:12:31 -0500

    feat: FINISHED and PASSED ALL tests_FFC - File Metadata Microservice

--- index.js
+++ index.js
@@ -2,15 +2,15 @@
 
 var express = require('express');
 var cors = require('cors');
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
 const multer = require('multer');
 require('dotenv').config()
 
-const upload = multer({ dest: 'uploads/' });
+const upload = multer({ dest: 'public/' });
 var app = express();
 
 app.use(cors());
-app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
@@ -18,10 +18,14 @@ app.get('/', function (req, res) {
 });
 
 
-
-app.post("/api/fileanalyse", (req, res) => {
+// -- POST request uploading local file with name, type, size in JSON response -- //
+app.post("/api/fileanalyse", upload.single('upfile'), (req, res) => {
+  console.log(req.file)
   console.log(req.body)
-  res.json({ success: true })
+  res.json({ 
+    name: req.file.originalname, 
+    type: req.file.mimetype, 
+    size: req.file.size })
 });
 
 
